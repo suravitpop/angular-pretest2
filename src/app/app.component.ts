aac169bf61eadc98434750b85c8c87328588981b
suravitpop <pop.suravit29@gmail.com>
2020-12-24 18:48:52 +0700

    whole

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pretest2';
+  quotes;
+  searchText: string;
+
+  constructor(private data: DataService) { }
+  ngOnInit() {
+    this.data.getQuotes().subscribe(data => {
+      this.quotes = data;
+
+      console.log(data);
+    });
+  }
 }
